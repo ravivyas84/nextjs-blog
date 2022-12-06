e9ea0431d13af4308a8c5227e15b08fe3a14b2ac
ravivyas84 <ravivyas84@gmail.com>
2022-12-06 23:05:19 +0530

    FIxed <a to <link

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
 
 export default function Header() {
   return (
   <div className="persona">
-      <span className="site-title"><a href="/">Ravi Vyas </a></span>
+      <span className="site-title"><Link href="/">Ravi Vyas </Link></span>
       <span className="site-description">Musings of a Learner</span>
       <div className="nav">
       <strong>&#123;</strong>
-      <a className="nav-item" href="/posts/">All Blogs</a> |
-      <a className="nav-item" href="/videos/">Videos</a> |
-      <a className="nav-item" target="_blank" rel="noopener noreferrer" href="https://multiplelenses.com/">Multiple Lenses</a> |
-      <a className="nav-item" target="_blank" rel="noopener noreferrer" href="https://ihatemyspreadsheet.com/">❤️ Spreadsheets</a> 
+      <Link className="nav-item" href="/posts/">All Blogs</Link> |
+      <Link className="nav-item" href="/videos/">Videos</Link> |
+      <Link className="nav-item" target="_blank" rel="noopener noreferrer" href="https://multiplelenses.com/">Multiple Lenses</Link> |
+      <Link className="nav-item" target="_blank" rel="noopener noreferrer" href="https://ihatemyspreadsheet.com/">❤️ Spreadsheets</Link> 
       <strong>&#125;</strong>
       </div>
   </div>)
