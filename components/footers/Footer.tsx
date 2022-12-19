7e046d8471e9eddc5f25f2886d8cf0b2d89f49b8
ravivyas84 <ravivyas84@gmail.com>
2022-12-19 15:14:36 +0530

    Update Footer.tsx
    added mastodon

--- components/footers/Footer.tsx
+++ components/footers/Footer.tsx
@@ -4,7 +4,11 @@ export default function Footer() {
   return (
     <div className={styles.container}>
       <p>
-        Built by <a href="https://twitter.com/ravivyas84"> Ravi Vyas</a>
+        Built by Ravi Vyas
+      </p>
+      <p>
+        <a rel="me" href="https://mastodon.social/@ravi">Mastodon</a>
+        <a href="https://twitter.com/ravivyas84">Twitter</a>
       </p>
       <p>
         Powered by <a href="https://nextjs.org/">NextJS</a>
