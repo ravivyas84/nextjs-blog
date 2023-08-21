f074411c9289dd175f23ac3eee3e95f4a05ee78c
ravivyas84 <ravivyas84@gmail.com>
2023-08-21 21:58:23 +0530

    Update layout.tsx
    updated umami tracker

--- components/layout.tsx
+++ components/layout.tsx
@@ -33,7 +33,7 @@ export default function Layout({
         /> */}
         {/* <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" /> */}
-        { env === 'development' ? "": <script async defer data-website-id="b2485249-6ade-4ba4-93f8-d9b89f3129f6" src="https://umami.ravivyas.com/umami.js"></script>}
+        { env === 'development' ? "": <script async src="https://umami.ravivyas.com/script.js" data-website-id="50b4b12b-136c-43fd-9ce3-f363e870995b"></script>}
         <link
           rel="preload"
           href="https://unpkg.com/prismjs@0.0.1/themes/prism-tomorrow.css"
